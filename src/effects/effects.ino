ffbb88738f6bf2747af156ef989c7d0d28d4d8f0
Kris <kris-krass>
2022-12-30 13:12:42 +0200

    update effects.ino

--- src/effects/effects.ino
+++ src/effects/effects.ino
@@ -14,57 +14,57 @@ void effederStep(bool boo, bool onof) {
   byte c = onof | (boo << 1);
 
   while (1) {
-    EACH_MILI(SPEED) {
-      counter++;
-      switch (effect_at_the_moment) {
-        case COLOR:
-          switch (c) {
-            case 0: usualStaticColor(1, 0, counter); break;
-            case 1: usualStaticColor(1, counter, NUM_OF_STEPS); break;
-            case 2: usualStaticColor(-1, NUM_OF_STEPS - counter, NUM_OF_STEPS); break;
-            case 3: usualStaticColor(-1, 0, NUM_OF_STEPS - counter); break;
-          }
-          break;
-        case RAINBOW:
-          switch (c) {
-            case 0: effectRainbow(-1, NUM_OF_STEPS - counter, NUM_OF_STEPS); break;
-            case 1: effectRainbow(-1, 0, NUM_OF_STEPS - counter); break;
-            case 2: effectRainbow(1, NUM_OF_STEPS - counter, NUM_OF_STEPS); break;
-            case 3: effectRainbow(1, 0, NUM_OF_STEPS - counter); break;
-          }
-          break;
-        case FIRE:
-          if (onof) {
-            int newBright = brightness_at_moment;
-            while (1) {
-              EACH_MILI(50) {
-                newBright -= 5;
-                if (newBright < 0) break;
-                strip.setBrightness(newBright);
-                stairsFire(0, 0, 0);
-                strip.show();
-              }
+      EACH_MILI(SPEED) {
+        counter++;
+        switch (effect_at_the_moment) {
+          case COLOR:
+            switch (c) {
+              case 0: usualStaticColor(1, 0, counter); break;
+              case 1: usualStaticColor(1, counter, NUM_OF_STEPS); break;
+              case 2: usualStaticColor(-1, NUM_OF_STEPS - counter, NUM_OF_STEPS); break;
+              case 3: usualStaticColor(-1, 0, NUM_OF_STEPS - counter); break;
             }
-            strip.clear();
-            strip.setBrightness(brightness_at_moment);
-            strip.show();
-          } else {
-            int newBright = 0;
-            strip.setBrightness(0);
-            while (1) {
-              EACH_MILI(50) {
-                newBright += 5;
-                if (newBright > brightness_at_moment) break;
-                strip.setBrightness(newBright);
-                stairsFire(0, 0, 0);
-                strip.show();
+            break;
+          case RAINBOW:
+            switch (c) {
+              case 0: effectRainbow(-1, NUM_OF_STEPS - counter, NUM_OF_STEPS); break;
+              case 1: effectRainbow(-1, 0, NUM_OF_STEPS - counter); break;
+              case 2: effectRainbow(1, NUM_OF_STEPS - counter, NUM_OF_STEPS); break;
+              case 3: effectRainbow(1, 0, NUM_OF_STEPS - counter); break;
+            }
+            break;
+          case FIRE:
+            if (onof) {
+              int newBright = brightness_at_moment;
+              while (1) {
+                EACH_MILI(50) {
+                  newBright -= 5;
+                  if (newBright < 0) break;
+                  strip.setBrightness(newBright);
+                  stairsFire(0, 0, 0);
+                  strip.show();
+                }
               }
+              strip.clear();
               strip.setBrightness(brightness_at_moment);
+              strip.show();
+            } else {
+              int newBright = 0;
+              strip.setBrightness(0);
+              while (1) {
+                EACH_MILI(50) {
+                  newBright += 5;
+                  if (newBright > brightness_at_moment) break;
+                  strip.setBrightness(newBright);
+                  stairsFire(0, 0, 0);
+                  strip.show();
+                }
+                strip.setBrightness(brightness_at_moment);
+              }
             }
-          }
-          return;
-          break;
-      }
+            return;
+            break;
+        }
       strip.show();
       if (counter == NUM_OF_STEPS) break;
     }
